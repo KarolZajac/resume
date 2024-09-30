f154e57b140369bda982d127a6ecfa7462a8f1fa
KarolZając <karol.zajac30@gmail.com>
2024-09-30 15:54:51 +0200

    Add scroll links, dark theme and fix responsiveness

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// menu
 const navToggle = document.querySelector('#nav-toggle'),
       navMenu = document.querySelector('#nav-menu');
 
@@ -15,3 +16,56 @@ const linkAction = function(){
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+// scroll links
+
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+
+window.addEventListener('scroll', scrollActive)
+
+// scroll top
+
+const scrollUp = () =>{
+	const scrollUp = document.querySelector('#scroll-top')
+  const scrollY = window.pageYOffset
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scrolltop')
+						: scrollUp.classList.remove('show-scrolltop')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+const themeButton = document.querySelector('#theme-button')
+
+const themeChange = () => {
+  if(themeButton.classList.contains('bxs-moon')){
+    themeButton.classList.remove('bxs-moon')
+    themeButton.classList.add('bx-sun')
+    document.querySelector('body').classList.add('dark-theme')
+  }
+  else{
+    themeButton.classList.remove('bx-sun')
+    themeButton.classList.add('bxs-moon')
+    document.querySelector('body').classList.remove('dark-theme')
+  }
+}
+
+themeButton.addEventListener('click', themeChange)
